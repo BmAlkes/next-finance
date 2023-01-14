c2de79c4def70f2ccb07bba507ac800cbead73a0
bmalkes <bmalkes@gmail.com>
2023-01-14 12:28:11 +0200

    feat categories

---
+++ source/Components/UI/CardsMenu.jsx
@@ -0,0 +1,10 @@
+import styles from "./CardMenu.module.scss";
+const CardsMenu = () => {
+  return (
+    <div className={styles.card}>
+      <div></div>
+    </div>
+  );
+};
+
+export default CardsMenu;
